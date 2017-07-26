7c9401344a81dd385bb195cd34b12bf94965899d
Theresa Ming <theresaming@gmail.com>
2017-07-26 15:17:53 -0700

    android

---
+++ server/vend.py
@@ -0,0 +1,22 @@
+# from pyduino import *
+import serial
+
+ser = serial.Serial("COM3", 9600, timeout = 1)
+
+#connected to the android
+connected = False
+while not connected:
+    fromAndroid = ser.read()
+    if fromAndroid == b'1':
+        connected = True
+
+# x = ser.write(b'1') #trigger android
+# print(x)
+ser.write(b'1')
+
+while True:
+    if (ser.read() != b'0'):
+        print(ser.read())
+        ser.write(b'1');
+
+ser.close()
