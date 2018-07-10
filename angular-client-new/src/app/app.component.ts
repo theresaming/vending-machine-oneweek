79db69bfaf858f58adb7bd59de7bf0837ceb4c8d
theresaming <theresaming@gmail.com>
2018-07-09 22:17:28 -0700

    broken but works

--- angular-client-new/src/app/app.component.ts
+++ angular-client-new/src/app/app.component.ts
@@ -1,30 +1,30 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
-
-// NEW CODE
 // import { Component } from '@angular/core';
-// // import { HeroService } from './hero.service';
-//
+
 // @Component({
-//     selector: 'app-root',
-//     template: `
-//         <router-outlet></router-outlet>
-//     `
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
 // })
 // export class AppComponent {
-// 	title = "Hello!";
-// 	// <slot-machine></slot-machine>
-//
-// 	onRated(event, progressBar): void {
-// 		console.log(event);
-// 		progressBar.reset();
-// 	}
+//   title = 'app';
 // }
+
+// NEW CODE
+import { Component } from '@angular/core';
+// import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'app-root',
+    template: `
+        <router-outlet></router-outlet>
+    `
+})
+export class AppComponent {
+	title = "Hello!";
+	// <slot-machine></slot-machine>
+
+	onRated(event, progressBar): void {
+		console.log(event);
+		progressBar.reset();
+	}
+}
