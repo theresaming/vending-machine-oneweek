c6608b7fb494c1868b15804f34a1a6fef48db9dc
theresaming <theresaming@gmail.com>
2018-07-18 15:35:36 -0700

    fixed asset links

--- flask-app/serverapi/views.py
+++ flask-app/serverapi/views.py
@@ -18,14 +18,13 @@ db = docdb.DocDbClient()
 # @app.route('/api/create/<string:db_id>/<string:coll_id>', methods=['POST'])
 # def create_doc(db_id, coll_id):
 #     # print(f'document to create: {request.json()["document"]}')
-#     # return jsonify(db.create_document(db_id, coll_id, request.json()["document"]))
+#     return jsonify(db.create_document(db_id, coll_id, request.get_json()["document"]))
 #     # return jsonify(db.create_document(db_id, coll_id, request.data))
-#     return "Hello There!"
 
 # @app.route('/api/upsert/<string:db_id>/<string:coll_id>', methods=['POST'])
 # def upsert_doc(db_id, coll_id):
 #     print(f'document to upsert: {request.get_json()["document"]}')
-#     return jsonify(db.upsert_document(db_id, coll_id, request.json()["document"]))
+#     return jsonify(db.upsert_document(db_id, coll_id, request.get_json()["document"]))
 
 # @app.route('/api/query/<string:db_id>/<string:coll_id>/<string:query>', methods=['GET'])
 # def query(db_id, coll_id, query):
