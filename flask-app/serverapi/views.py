e81241bb09fe2cc50e48d6987f048c3510af4bce
Matthew Turner <matthewallenturner@gmail.com>
2018-07-18 18:17:12 -0700

    lottery class

--- flask-app/serverapi/views.py
+++ flask-app/serverapi/views.py
@@ -17,15 +17,14 @@ def delete_doc(db_id, coll_id, doc_id):
 
 # @app.route('/api/create/<string:db_id>/<string:coll_id>', methods=['POST'])
 def create_doc(db_id, coll_id):
-    # print(f'document to create: {request.json()["document"]}')
-    return jsonify(db.create_document(db_id, coll_id, request.json["document"]))
-    # return jsonify(db.create_document(db_id, coll_id, request.data))
+    print(f'document to create: {request.json()["document"]}')
+    return jsonify(db.create_document(db_id, coll_id, request.get_json()["document"]))
 
 # @app.route('/api/upsert/<string:db_id>/<string:coll_id>', methods=['POST'])
 def upsert_doc(db_id, coll_id):
-    print(request.json["document"])
-    return jsonify(db.upsert_document(db_id, coll_id, request.json["document"]))
+    print(request.get_json()["document"])
+    return jsonify(db.upsert_document(db_id, coll_id, request.get_json()["document"]))
 
 # @app.route('/api/query/<string:db_id>/<string:coll_id>/<string:query>', methods=['GET'])
-# def query(db_id, coll_id, query):
-    # return jsonify(db.query(db_id, coll_id, f'select * from c where {query}'))
+def query(db_id, coll_id, query):
+    return jsonify(db.query(db_id, coll_id, 'select * from c where {}'.format(query)))
---
+++ scripts/vending/lottery.py
@@ -0,0 +1,34 @@
+from random import randint
+
+VAL_BOX = 4
+VAL_WHEEL = 5
+VAL_COIL = 6
+VAL_CONVEYOR = 7
+
+NUM_BOX = 30
+NUM_WHEEL = 14
+NUM_COIL = 7
+NUM_CONVEYOR = 12
+
+class Lottery:
+    __items = []
+
+    def reset(self):
+        self.__items = [VAL_BOX]*NUM_BOX + [VAL_WHEEL]*NUM_WHEEL + [VAL_COIL]*NUM_COIL + [VAL_CONVEYOR]*NUM_CONVEYOR
+
+    def spin(self):
+        if len(self.__items) == 0:
+            return -1
+        return self.__items.pop(randint(0, len(self.__items)-1))
+
+    def get_num_items(self):
+        return len(self.__items)
+
+    def __init__(self):
+        self.reset()
+
+if __name__ == "__main__":
+    lott = Lottery()
+    print("running")
+    for i in range(lott.get_num_items() + 4):
+        print(lott.spin())
