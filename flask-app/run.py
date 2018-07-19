07479946ba6d15b4352a89c224a2c11a4c8eda94
theresaming <theresaming@gmail.com>
2018-07-18 18:19:40 -0700

    query db ref images

--- flask-app/run.py
+++ flask-app/run.py
@@ -1,5 +1,6 @@
-from serverapi import app
-from flask import Flask, render_template, url_for
+from serverapi import views
+from flask import Flask, render_template, url_for, make_response
+import random
 
 app = Flask(__name__)
 
@@ -9,7 +10,17 @@ def home():
 
 @app.route("/rate")
 def evaluate_images():
-    return render_template("image-rating.html", category = "Adidas")
+    # print (views.get_doc("data", "categories", "4"))
+    category = get_ref()[0]
+    ref_img_arr = get_ref()[1]
+    return render_template("image-rating.html", category = category, ref_img_arr = ref_img_arr)
+
+def get_ref():
+    randomInteger = str(random.randint(0,20))
+    value = views.get_doc("data", "categories", randomInteger)
+    return [value['name'], value['ref_image_paths']]
+    # return get_doc(db_id, coll_id, doc_id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
