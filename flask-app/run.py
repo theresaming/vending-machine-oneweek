7d9ce0e090714fcc2efe1630d3d0bf93d4e8caa3
theresaming <theresaming@gmail.com>
2018-07-18 19:45:24 -0700

    query ver images

--- flask-app/run.py
+++ flask-app/run.py
@@ -11,10 +11,16 @@ def home():
 @app.route("/rate")
 def evaluate_images():
     # print (views.get_doc("data", "categories", "4"))
-    info = get_ref()
-    category = info[0]
-    ref_img_arr = info[1]
-    return render_template("image-rating.html", category = category, ref_img_arr = ref_img_arr)
+    reference = get_ref()
+    category = reference[0]
+    ref_img_arr = reference[1]
+    ver_img_arr = get_verify_images(category)
+    ver_url_arr = []
+    for image in ver_img_arr:
+        ver_url_arr.append(image['img_url'])
+    print (ver_url_arr)
+
+    return render_template("image-rating.html", category = category, ref_img_arr = ref_img_arr, ver_img_arr = ver_url_arr)
 
 def get_ref():
     randomInteger = str(random.randint(0,20))
@@ -22,8 +28,13 @@ def get_ref():
     return [value['name'], value['ref_image_paths']]
     # return get_doc(db_id, coll_id, doc_id)
 
-# def get_verify_images():
-
+def get_verify_images(category):
+    query = "select * from c where c.category=\"{}\" order by c.judgement_count asc".format(category)
+    img_json = views.query("data", "images", query)
+    ver_img_arr = []
+    for i in range(6):
+        ver_img_arr.append(img_json[i])
+    return ver_img_arr
 
 
 if __name__ == "__main__":
